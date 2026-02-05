8da21c137f549361083f03e3ad288f7240549d07
Nricks07 <enriquelr2804@gmail.com>
2026-02-05 09:31:56 -0600

    Delete piton directory

--- piton/main_invent.py
+++
@@ -1,9 +0,0 @@
-from SistemaInven import *
-
-prod1 = Producto("Maruchan", 16, 100)
-prod1.apli_desc(0.30)
-prod1.actualizar_stock(-120)
-prod1.actualizar_stock(120)
-prod1.actualizar_stock(100)
-
-prod2 = Celular("chaifon", 10, 10, )
